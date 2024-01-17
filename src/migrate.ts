025e3ac5c99e9e09b04dd3f52a65d466cde574a7
Leopold Bauer <leopold.bauer@titanom.com>
2024-01-17 18:12:21 +0100

    ignore migrate

--- src/migrate.ts
+++ src/migrate.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
+import { database_url } from "./const";
 
 const databaseUrl = drizzle(postgres(database_url, { ssl: "require", max: 1 }));
 
